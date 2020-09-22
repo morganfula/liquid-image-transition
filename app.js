ba985e929c67268b69591ece79b0c01086b31e9b
morganfula <morgan.fula@me.com>
2020-09-22 13:55:04 +0200

    done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 new hoverEffect({
   parent: document.querySelector('.distortion'),
-  intensity: 0.2,
-  image1: '.girl1.jpg',
-  image2: '.girl2.jpg',
-  displacementIamge: './fx.png'
+  intensity: 0.1,
+  image1: './bw1.jpg',
+  image2: './bw2.jpg',
+  speedIn: 5,
+  // angle: Math.PI / 38,
+
+  displacementImage: './fx.png'
 });
